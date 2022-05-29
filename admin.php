98af382e4563de6970b79546b8ef78f3f0cf2492
Manuel <manuel.castro.r.1@gmail.com>
2022-05-28 21:22:35 -0500

    apis

--- admin.php
+++ admin.php
@@ -43,24 +43,171 @@
 
  function AddAnimal(){
 
-   var animalId = document.getElementById('AId').value;
-   var animalName = document.getElementById('AName').value;
-   var animalSpecie = document.getElementById('Specie').value;
-   var animalAge = document.getElementById('AnimalAge').value;
-   var animalGender = document.getElementById('AGender').value;
+   var AId = document.getElementById('AId').value;
+   var Anombre = document.getElementById('AName').value;
+   var specie = document.getElementById('Specie').value;
 
-   const information = 'info={"name":"John", "birth":"1986-12-14", "city":"New York"}';
+   var AGender = document.getElementById('AGender').value;
+
+   const info = '{"id":"'+AId+'", "nombre":"'+Anombre+'", "sexo":"'+AGender+'","especie":"'+specie+'"}';
+
+   const obj = JSON.parse(info);
+
+
    //const obj = JSON.parse(text);
    $.ajax({
-           url:'addorder.php',
+           url:'https://ap-zoo-animal-api.herokuapp.com/createAnimal',
            type:'post',
-           data:information,
+           data:obj,
            success:function(data){
-             alert(data);
+
+             if (data.error ==null){
+             swal("Ok", "Registro agregado correctamente!", "success");
+               }
+               else{
+                 swal("Something went wrong", "No se pudo actualizar el registro :(", "error");
+               }
 
            }
        });
  }
+
+ function AddEmployee(){
+
+   var EId = document.getElementById('EId').value;
+   var Enombre = document.getElementById('Name').value;
+   var apellido = document.getElementById('LN').value;
+   var edad = document.getElementById('Age').value;
+   var EGender = document.getElementById('Gender').value;
+
+   const info = '{"id":"'+EId+'", "nombre":"'+Enombre+'", "apellido":"'+apellido+'", "edad":"'+edad+'", "sexo":"'+EGender+'"}';
+
+   const obj = JSON.parse(info);
+
+
+   $.ajax({
+           url:'https://ap-zoo-employee-api.herokuapp.com/createEmployee',
+           type:'post',
+           data:obj,
+           success:function(data){
+
+            if (data.error ==null){
+            swal("Ok", "Registro actualizado correctamente!", "success");
+              }
+              else{
+                swal("Something went wrong", "No se pudo actualizar el registro :(", "error");
+              }
+
+           }
+       });
+ }
+
+
+ function UpdateEmployees(){
+
+   var EId = document.getElementById('EId').value;
+   var Enombre = document.getElementById('Name').value;
+   var apellido = document.getElementById('LN').value;
+   var edad = document.getElementById('Age').value;
+   var EGender = document.getElementById('Gender').value;
+
+   const info = '{"id":"'+EId+'", "nombre":"'+Enombre+'", "apellido":"'+apellido+'", "edad":"'+edad+'", "sexo":"'+EGender+'"}';
+
+   const obj = JSON.parse(info);
+
+
+   $.ajax({
+           url:'https://ap-zoo-employee-api.herokuapp.com/updateEmployee',
+           type:'put',
+           data:obj,
+           success:function(data){
+
+             if (data.error ==null){
+             swal("Ok", "Registro actualizado correctamente!", "success");
+               }
+               else{
+                 swal("Something went wrong", "No se pudo actualizar el registro :(", "error");
+               }
+           }
+       });
+ }
+
+ function UpdateAnimal(){
+
+   var AId = document.getElementById('AId').value;
+   var Anombre = document.getElementById('AName').value;
+   var specie = document.getElementById('Specie').value;
+
+   var AGender = document.getElementById('AGender').value;
+
+   const info = '{"id":"'+AId+'", "nombre":"'+Anombre+'", "sexo":"'+AGender+'","especie":"'+specie+'"}';
+
+   const obj = JSON.parse(info);
+
+
+   $.ajax({
+           url:'https://ap-zoo-animal-api.herokuapp.com/updateAnimal',
+           type:'put',
+           data:obj,
+           success:function(data){
+
+             if (data.error ==null){
+             swal("Ok", "Registro actualizado correctamente!", "success");
+               }
+               else{alert(data.error);}
+           }
+       });
+ }
+
+ function DeleteEmployee(){
+
+   var EId = document.getElementById('EId').value;
+
+
+   const info = '{"id":"'+EId+'"}';
+
+   const obj = JSON.parse(info);
+
+
+   $.ajax({
+           url:'https://ap-zoo-employee-api.herokuapp.com/deleteEmployee',
+           type:'delete',
+           data:obj,
+           success:function(data){
+
+             if (data.error ==null){
+             swal("Ok", "Registro eliminado correctamente!", "success");
+               }
+               else{alert(data.error);}
+           }
+       });
+ }
+
+ function DeleteAnimal(){
+
+   var AId = document.getElementById('AId').value;
+
+   const info = '{"id":"'+AId+'"}';
+
+   const obj = JSON.parse(info);
+
+   $.ajax({
+           url:'https://ap-zoo-animal-api.herokuapp.com/deleteAnimal',
+           type:'delete',
+           data:obj,
+           success:function(data){
+
+             if (data.error ==null){
+             swal("Ok", "Animal eliminado correctamente!", "success");
+               }
+               else{alert(data.error);}
+           }
+       });
+ }
+
+
+
+
 
  function ConsultAllAnimals(){
    window.location.href = "showallanimals.php";
@@ -94,7 +241,7 @@
      <input type="text" class="inputfield"  placeholder="Gender" name ="Gender" id = "Gender"/> </br></br>
 
      <div class="button2" onclick="AddEmployee()">Add</div>
-     <div class="button2" onclick="SingleConsultEmployees()">Consult </div>
+     <div class="button2" onclick="UpdateEmployees()">Update </div>
      <div class="button2" onclick="ConsultAllEmployees()">Consult All </div>
      <div class="button2" onclick="DeleteEmployee()">Delete</div>
 
@@ -119,11 +266,11 @@
      <input type="text" class="inputfield"  placeholder="Animal Id" name ="AId" id = "AId"/> </br></br>
      <input type="text" class="inputfield"  placeholder="Name" name ="AName" id = "AName"/> </br></br>
      <input type="text" class="inputfield"  placeholder="Specie" name ="Specie" id = "Specie"/> </br></br>
-     <input type="text" class="inputfield"  placeholder="Age" name ="AnimalAge" id = "AnimalAge"/> </br></br>
+
      <input type="text" class="inputfield"  placeholder="Gender" name ="AGender" id = "AGender"/> </br></br>
 
      <div class="button2" onclick="AddAnimal()">Add</div>
-     <div class="button2" onclick="SingleConsultAnimal()">Consult </div>
+     <div class="button2" onclick="UpdateAnimal()">Update </div>
      <div class="button2" onclick="ConsultAllAnimals()">Consult All </div>
      <div class="button2" onclick="DeleteAnimal()">Delete</div>
 
--- showallemployees.php
+++ showallemployees.php
@@ -13,75 +13,76 @@
 
     <script>
     function CreateTableFromJSON() {
-        var myBooks = [
-            {
-                "Book ID": "1",
-                "Book Name": "Computer Architecture",
-                "Category": "Computers",
-                "Price": "125.60",
-                "Country":"Colombia"
-            },
-            {
-                "Book ID": "2",
-                "Book Name": "Asp.Net 4 Blue Book",
-                "Category": "Programming",
-                "Price": "56.00",
-                "Country":"Colombia"
-            },
-            {
-                "Book ID": "3",
-                "Book Name": "Popular Science",
-                "Category": "Science",
-                "Price": "210.40",
-                "Country":"Colombia"
-            }
-        ]
 
-        // EXTRACT VALUE FOR HTML HEADER.
-        // ('Book ID', 'Book Name', 'Category' and 'Price')
-        var col = [];
-        for (var i = 0; i < myBooks.length; i++) {
-            for (var key in myBooks[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
+
+
+      $.ajax({
+              url:'https://ap-zoo-employee-api.herokuapp.com/readEmployees',
+              type:'get',
+              success:function(data){
+
+
+                const myBooks = JSON.parse(JSON.stringify(data.rows));
+
+                var col = [];
+                for (var i = 0; i < myBooks.length; i++) {
+                    for (var key in myBooks[i]) {
+                        if (col.indexOf(key) === -1) {
+                            col.push(key);
+                        }
+                    }
                 }
-            }
-        }
 
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
+                // CREATE DYNAMIC TABLE.
+                var table = document.createElement("table");
 
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+                // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-        var tr = table.insertRow(-1);                   // TABLE ROW.
+                var tr = table.insertRow(-1);                   // TABLE ROW.
 
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
+                for (var i = 0; i < col.length; i++) {
+                    var th = document.createElement("th");      // TABLE HEADER.
+                    th.innerHTML = col[i];
+                    tr.appendChild(th);
+                }
 
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < myBooks.length; i++) {
+                // ADD JSON DATA TO THE TABLE AS ROWS.
+                for (var i = 0; i < myBooks.length; i++) {
 
-            tr = table.insertRow(-1);
+                    tr = table.insertRow(-1);
 
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = myBooks[i][col[j]];
-            }
-        }
+                    for (var j = 0; j < col.length; j++) {
+                        var tabCell = tr.insertCell(-1);
+                        tabCell.innerHTML = myBooks[i][col[j]];
+                    }
+                }
 
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
+                // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+                var divContainer = document.getElementById("showData");
+                divContainer.innerHTML = "";
+                divContainer.appendChild(table);
+
+
+
+
+              }
+          });
+
+
       }
     </script>
 </head>
 <body>
 
-    <p id="showData"></p>
+  <p class="tituloensayo ">Employees</p>
+
+    <p id="showData"></p> <br><br>
+
+    <center>
+
+    <a href="admin.php">Volver </a>
+
+  </center>
 </body>
 
 <script>
